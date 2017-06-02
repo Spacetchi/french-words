297bc81b69ff4fdd3fe8e9e011ea118e75c5d42e
spacetchi <medusa.moxie@protonmail.com>
2017-06-01 20:10:38 -0700

    Add existing files for grade 10 and 11

---
+++ french_lists_10.js
@@ -0,0 +1,861 @@
+var list_house = [
+	
+	{ en: "a balcony", 
+	fr: "un balcon" },
+	
+	{ en: "an apartment", 
+	fr: "un appartement" },
+	
+	{ en: "a basement or cellar",
+	fr: "une cave" },
+	
+	{ en: "to rent",
+	fr: "louer" },
+	
+	{ en: "a hallway",
+	fr: "un couloir" },
+	
+	{ en: "a kitchen",
+	fr: "une cuisine" },
+	
+	{ en: "a staircase",
+	fr: "un escalier" },
+	
+	{ en: "a building",
+	fr: "un immeuble" }, 
+	
+	{ en: "a yard or garden",
+	fr: "un jardin" },
+	
+	{ en: "(a) housing",
+	fr: "un logement" },
+	
+	{ en: "(a) rent",
+	fr: "un loyer" }, 
+	
+	{ en: "a neighbourhood",
+	fr: "un quartier" },
+	
+	{ en: "a living room (not la salle de séjour)",
+	fr: "un salon"},
+	
+	{ en: "a studio",
+	fr: "un studio" },
+	
+	{ en: "an armoire",
+	fr: "un armoir" },
+	
+	{ en: "a shower",
+	fr: "une douche" }, 
+	
+	{ en: "a sink",
+	fr: "un lavabo" },
+	
+	{ en: "a piece of furniture",
+	fr: "un meuble" },
+	
+	{ en: "a closet or cupboard",
+	fr: "un placard" },
+	
+	{ en: "a drawer",
+	fr: "un tiroir" }, 
+	
+	{ en: "the toilets",
+	fr: "les toilettes" },
+	
+	{ en: "a bathroom",
+	fr: "une salle de bains" },
+	
+	{ en: "a mirror",
+	fr: "un miroir" },
+	
+	{ en: "a wall",
+	fr: "un mur" },
+	
+	{ en: "the curtains",
+	fr: "les rideaux" },
+	
+	{ en: "a poster",
+	fr: "une affiche" },
+	
+	{ en: "a chest of drawers",
+	fr: "une commode" },
+	
+	{ en: "a bedroom",
+	fr: "une chambre" },
+	
+	{ en: "a flower",
+	fr: "une fleur" },
+	
+	{ en: "a lamp",
+	fr: "une lampe" },
+	
+	{ en: "an armchair",
+	fr: "un fauteuil" },
+	
+	{ en: "a bathtub",
+	fr: "une baignoire" },
+	
+	{ en: "a basement",
+	fr: "un sous-sol" },
+	
+	{ en: "a garage",
+	fr: "un garage" },
+	
+	{ en: "to move out",
+	fr: "déménager" },
+	
+	{ en: "to move in",
+	fr: "emménager" },
+	
+	{ en: "a room",
+	fr: "une pièce" },
+	
+	{ en: "a dorm",
+	fr: "une résidence universitaire" },
+	
+	{ en: "a dining room",
+	fr: "une salle à manger" },
+	
+	{ en: "a property owner (male)",
+	fr: "un propriétaire" },
+	
+	{ en: "a shelf",
+	fr: "une étagère" },
+	
+	{ en: "a couch",
+	fr: "un canapé" },
+	
+	{ en: "a living room (not un salon)",
+	fr: "une salle de séjour" }
+	
+];
+
+var list_reflex = [
+
+    { en: "to brush one's hair",
+    fr: "se brosser les cheveux" },
+  
+    { en: "to brush one's teeth",
+    fr: "se brosser les dents" },
+
+    { en: "to do one's hair",
+    fr: "se coiffer" },
+  
+    { en: "to go to bed",
+    fr: "se coucher" },
+  
+    { en: "to undress oneself",
+    fr: "se déshabiller" },
+  
+    { en: "to go to sleep, to fall asleep",
+    fr: "s'endormir" },
+  
+    { en: "to get dressed",
+    fr: "s'habiller" },
+  
+    { en: "to wash oneself",
+    fr: "se laver" },
+
+    { en: "to look at oneself",
+    fr: "se regarder" },
+  
+    { en: "to wake up",
+    fr: "se réveiller" },
+  
+    { en: "to dry oneself",
+    fr: "se sécher" },
+	
+    { en: "to shave",
+    fr: "se raser" },
+	
+    { en: "to apply makeup",
+    fr: "se maquiller" },
+	
+    { en: "to stand up",
+    fr: "se lever" },
+	
+	{ en: "to have fun",
+	fr: "s'amuser" },
+	
+	{ en: "to be called",
+	fr: "s'appeler" },
+	
+	{ en: "to stop",
+	fr: "s'arrêter" },
+	
+	{ en: "to sit down",
+	fr: "s'asseoir" },
+	
+	{ en: "to hurry",
+	fr: "se dépêcher" },
+	
+	{ en: "to relax",
+	fr: "se détendre" },
+	
+	{ en: "to argue with",
+	fr: "se disputer avec" },
+	
+	{ en: "to become upset",
+	fr: "s'énerver" },
+	
+	{ en: "to get bored",
+	fr: "s'ennuyer" },
+	
+	{ en: "to get along well with",
+	fr: "bien s'entendre avec" },
+	
+	{ en: "to worry",
+	fr: "s'inquiéter" },
+	
+	{ en: "to be interested in",
+	fr: "s'intéresser à" },
+	
+	{ en: "to begin to",
+	fr: "se mettre à" },
+	
+	{ en: "to become angry",
+	fr: "se mettre en colère" },
+	
+	{ en: "to occupy oneself",
+	fr: "s'occuper" },
+	
+	{ en: "to get ready",
+	fr: "se préparer" },
+	
+	{ en: "to take a walk",
+	fr: "se promener" },
+
+	{ en: "to realise",
+	fr: "se rendre compte" },
+	
+	{ en: "to rest",
+	fr: "se reposer" },
+	
+	{ en: "to remember",
+	fr: "se souvenir" },
+	
+	{ en: "to be mistaken",
+	fr: "se tromper" },
+	
+	{ en: "to be located",
+	fr: "se trouver" },
+	
+	{ en: "to sprain",
+	fr: "se fouler" },
+	
+	{ en: "to hurt oneself",
+	fr: "se blesser" },
+	
+	{ en: "to break oneself",
+	fr: "se casser" },
+	
+	{ en: "to be ill",
+	fr: "se porter mal" },
+	
+	{ en: "to be better",
+	fr: "se porter mieux" },
+	
+	{ en: "to feel",
+	fr: "se sentir" }
+	
+];
+
+var list_routine = [
+  
+    { en: "to wash up",
+    fr: "faire sa toilette" },
+  
+    { en: "to take a shower",
+    fr: "prendre une douche" },
+  
+    { en: "(the) shampoo",
+    fr: "le shampooing" },
+  
+    { en: "the heart",
+    fr: "le cœur" },
+      
+    { en: "the body",
+    fr: "le corps" },
+      
+    { en: "the back",
+    fr: "le dos" },
+      
+    { en: "the throat",
+    fr: "la gorge" },
+      
+    { en: "a cheek",
+    fr: "un joue" },
+      
+    { en: "a toe",
+    fr: "un orteil" },
+      
+    { en: "the skin",
+    fr: "la peau" },
+      
+    { en: "the chest",
+    fr: "la poitrine" },
+      
+    { en: "the waist",
+    fr: "la taille" },
+      
+    { en: "the face",
+    fr: "le visage" },
+      
+    { en: "a toothbrush",
+    fr: "une brosse à dents" },
+      
+    { en: "(the) toothpaste",
+    fr: "le dentifrice" },
+      
+    { en: "a rasor",
+    fr: "un rasoir" },
+ 
+    { en: "a slipper",
+    fr: "un pantoufle" },
+      
+    { en: "a comb",
+    fr: "un peigne" },
+      
+    { en: "(the) soap",
+    fr: "le savon" },
+      
+    { en: "a bath towel",
+    fr: "une serviette de bain" },
+      
+    { en: "a hairbrush",
+    fr: "une brosse à cheveux" },
+      
+    { en: "(the) makeup",
+    fr: "le maquillage" },
+      
+    { en: "the head",
+    fr: "la tête" },
+      
+    { en: "the mouth",
+    fr: "la bouche" },
+      
+    { en: "the nose",
+    fr: "le nez" },
+      
+    { en: "an eye",
+    fr: "un œil" },
+      
+    { en: "an arm",
+    fr: "un bras" },
+      
+    { en: "the neck",
+    fr: "le cou" },
+      
+    { en: "a finger",
+    fr: "un doigt" },
+      
+    { en: "the stomach",
+    fr: "le ventre" },
+      
+    { en: "a knee",
+    fr: "un genou" },
+      
+    { en: "a leg",
+    fr: "une jambe" },
+      
+    { en: "a foot",
+    fr: "un pied" },
+      
+    { en: "an alarm clock",
+    fr: "un réveil" }
+      
+];
+
+var list_chores = [
+
+	{ en: "to clear the table",
+	fr: "débarraser la table" },
+	
+	{ en: "to dust",
+	fr: "faire la poussière" },
+	
+	{ en: "to dry the dishes",
+	fr: "essuyer la vaiselle" },
+	
+	{ en: "to wipe the table",
+	fr: "essuyer la table" },
+	
+	{ en: "to do the laundry",
+	fr: "faire la lessive" },
+	
+	{ en: "to do the housework",
+	fr: "faire le ménage" },
+	
+	{ en: "to wash",
+	fr: "laver" },
+	
+	{ en: "to set the table",
+	fr: "mettre la table" },
+	
+	{ en: "to vacuum",
+	fr: "passer l'aspirateur" },
+	
+	{ en: "to tidy up",
+	fr: "ranger" },
+	
+	{ en: "to make dirty",
+	fr: "salir" },
+	
+	{ en: "clean (adj)",
+	fr: "proper" },
+	
+	{ en: "dirty (adj)",
+	fr: "sale" },
+	
+	{ en: "an electrical appliance",
+	fr: "un appareil électrique" },
+	
+	{ en: "a household appliance",
+	fr: "un appareil ménager" },
+	
+	{ en: "a coffeemaker",
+	fr: "une cafetière" },
+	
+	{ en: "a stove",
+	fr: "une cuisinière" },
+	
+	{ en: "a toaster",
+	fr: "un grille-pain" },
+	
+	{ en: "a washing machine",
+	fr: "un lave-linge" },
+	
+	{ en: "a dishwasher",
+	fr: "un lave-vaiselle" },
+	
+	{ en: "a clothes dryer",
+	fr: "un sèche-linge" },
+	
+	{ en: "a household chore",
+	fr: "une tâche ménagère" },
+	
+	{ en: "a kitchen sink",
+	fr: "un évier" },
+	
+	{ en: "a microwave",
+	fr: "un micro-ondes" },
+	
+	{ en: "to make the bed",
+	fr: "faire le lit" },
+	
+	{ en: "a pillow",
+	fr: "un oreiller" },
+	
+	{ en: "the drapes",
+	fr: "les draps" },
+	
+	{ en: "a blanket",
+	fr: "une couverture" },
+	
+	{ en: "an oven",
+	fr: "un four" },
+	
+	{ en: "to sweep",
+	fr: "balayer" },
+	
+	{ en: "the laundry",
+	fr: "le linge" },
+	
+	{ en: "an iron",
+	fr: "un fer à repasser" },
+	
+	{ en: "to iron",
+	fr: "repasser" },
+	
+	{ en: "to take out the trash",
+	fr: "sortir la poubelle" },
+	
+	{ en: "a freezer",
+	fr: "un congélateur" },
+	
+	{ en: "a fridge",
+	fr: "un frigo" }
+
+];
+
+var list_food = [
+
+	{ en: "to cook",
+	fr: "cuisiner" },
+	
+	{ en: "to go grocery shopping",
+	fr: "faire les courses" },
+	
+	{ en: "a school cafeteria",
+	fr: "une cantine" },
+	
+	{ en: "a supermarket",
+	fr: "un supermarché" },
+	
+	{ en: "a food item",
+	fr: "un aliment" },
+	
+	{ en: "(a) lunch",
+	fr: "un déjeuner" },
+	
+	{ en: "(a) dinner",
+	fr: "un dîner" },
+	
+	{ en: "a snack",
+	fr: "un goûter" },
+	
+	{ en: "(the) food or sustenance",
+	fr: "la nourriture" },
+	
+	{ en: "(a) breakfast",
+	fr: "un petit-déjeuner" },
+	
+	{ en: "a meal",
+	fr: "un repas" },
+	
+	{ en: "(the) peas",
+	fr: "les petits pois" },
+	
+	{ en: "a salad",
+	fr: "une salad" },
+	
+	{ en: "(the) beef",
+	fr: "le bœuf" },
+	
+	{ en: "an esgarcot",
+	fr: "un escargot" },
+	
+	{ en: "(the) seafood",
+	fr: "les fruits de mer" },
+	
+	{ en: "(a) pâté",
+	fr: "un pâté" },
+	
+	{ en: "(the) pork",
+	fr: "le porc" },
+	
+	{ en: "a chicken",
+	fr: "un poulet" },
+	
+	{ en: "a sausage",
+	fr: "une saucisse" },
+	
+	{ en: "a steak",
+	fr: "un steak" },
+	
+	{ en: "(the) tuna",
+	fr: "le thon" },
+	
+	{ en: "(the) meat",
+	fr: "la viande" },
+	
+	{ en: "(the) rice",
+	fr: "le riz" },
+	
+	{ en: "(the) pasta",
+	fr: "les pâtes" },
+	
+	{ en: "(a) yogurt",
+	fr: "un yaourt" },
+	
+	{ en: "a fruit",
+	fr: "un fruit" },
+	
+	{ en: "a pear",
+	fr: "une poire" },
+	
+	{ en: "an orange",
+	fr: "une orange" },
+	
+	{ en: "a strawberry",
+	fr: "une fraise" },
+	
+	{ en: "a peach",
+	fr: "une pêche" },
+	
+	{ en: "a banana",
+	fr: "une banane" },
+	
+	{ en: "an apple",
+	fr: "une pomme" },
+	
+	{ en: "a vegetable",
+	fr: "un légume" },
+	
+	{ en: "a potato",
+	fr: "une pomme de terre" },
+	
+	{ en: "a carrot",
+	fr: "une carotte" },
+	
+	{ en: "an onion",
+	fr: "un oignon" },
+	
+	{ en: "a red pepper",
+	fr: "un poivron rouge" },
+	
+	{ en: "a green bean",
+	fr: "un haricot vert" },
+
+	{ en: "(the) garlic",
+	fr: "l'ail" },
+	
+	{ en: "a mushroom",
+	fr: "un champignon" },
+	
+	{ en: "a tomato",
+	fr: "une tomate" },
+	
+	{ en: "(the) lettuce",
+	fr: "la laitue" },
+	
+	{ en: "an egg",
+	fr: "un œuf" },
+	
+	{ en: "(the) strawberry jam",
+	fr: "la confiture de fraises" },
+	
+	{ en: "a strawberry pie",
+	fr: "une tarte aux fraises" }
+	
+];
+
+var list_resto = [
+
+	{ en: "to be on a diet",
+	fr: "être au régime" },
+	
+	{ en: "a can",
+	fr: "une boîte" },
+	
+	{ en: "the cream",
+	fr: "la crème" },
+	
+	{ en: "(the) mayonnaise",
+	fr: "la mayonnaise" },
+	
+	{ en: "(the) mustard",
+	fr: "la moutard" },
+	
+	{ en: "a slice",
+	fr: "une tranche" },
+	
+	{ en: "an appetizer",
+	fr: "une entrée" },
+	
+	{ en: "an appetizer (not une entrée)",
+	fr: "un hors-d'œuvre" },
+	
+	{ en: "a dish (food)",
+	fr: "un plat" },
+	
+	{ en: "the main dish",
+	fr: "le plat principal" },
+	
+	{ en: "Let's eat!",
+	fr: "À table!" },
+	
+	{ en: "included",
+	fr: "compris" },
+	
+	{ en: "a butcher's shop",
+	fr: "une boucherie" },
+	
+	{ en: "a bread shop",
+	fr: "une boulangerie" },
+	
+	{ en: "a delicatessen",
+	fr: "une charcuterie" },
+	
+	{ en: "a shopkeeper",
+	fr: "un commerçant" },
+	
+	{ en: "a kilogram",
+	fr: "un kilogramme" },
+	
+	{ en: "a pastry shop",
+	fr: "une pâtisserie" },
+	
+	{ en: "a pastry",
+	fr: "une pâtisserie" },
+	
+	{ en: "a fish shop",
+	fr: "une poissonnerie" },
+	
+	{ en: "a plate",
+	fr: "une assiette" },
+	
+	{ en: "a menu (not un menu)",
+	fr: "une carte" },
+	
+	{ en: "a menu (not une carte)",
+	fr: "un menu" },
+	
+	{ en: "to taste",
+	fr: "goûter" },
+	
+	{ en: "to order",
+	fr: "commander" },
+
+	{ en: "(the) pepper (condiment)",
+	fr: "le poivre" },
+	
+	{ en: "(the) salt",
+	fr: "le sel" },
+	
+	{ en: "a carafe",
+	fr: "une carafe" },
+	
+	{ en: "a napkin",
+	fr: "une serviette" },
+	
+	{ en: "a fork",
+	fr: "une fourchette" },
+	
+	{ en: "a knife",
+	fr: "un couteau" },
+	
+	{ en: "a tablecloth",
+	fr: "une nappe" },
+	
+	{ en: "(the) olive oil",
+	fr: "l'huile d'olive" },
+	
+	{ en: "a bowl",
+	fr: "un bol" },
+	
+	{ en: "a table spoon",
+	fr: "une cuillère à soupe" },
+	
+	{ en: "a tea spoon",
+	fr: "une cuillère à café" }
+	
+];
+
+var list_illness = [
+
+	{ en: "to go to the ER",
+	fr: "aller aux urgences" },
+	
+	{ en: "to go to the pharmacy",
+	fr: "aller à la pharmacie" },
+	
+	{ en: "to have an ache",
+	fr: "avoir mal" },
+	
+	{ en: "to feel nauseated",
+	fr: "avoir mal au coeur" },
+	
+	{ en: "to swell",
+	fr: "enfler" },
+	
+	{ en: "to be in good health",
+	fr: "être en bonne santé" },
+	
+	{ en: "to be in bad health",
+	fr: "être en mauvaise santé" },
+	
+	{ en: "to be in good shape",
+	fr: "être en plein forme" },
+	
+	{ en: "to avoid",
+	fr: "éviter" },
+	
+	{ en: "to (feel) hurt",
+	fr: "faire mal" },
+	
+	{ en: "to stay slim",
+	fr: "garder la ligne" },
+	
+	{ en: "to get better",
+	fr: "guérir" },
+	
+	{ en: "to fall sick",
+	fr: "tomber malade" },
+	
+	{ en: "to be sick",
+	fr: "être malade" },
+	
+	{ en: "a dentist",
+	fr: "un dentiste" },
+	
+	{ en: "a pharmacist",
+	fr: "un pharmacien" },
+	
+	{ en: "an allergy",
+	fr: "une allergie" },
+	
+	{ en: "a pain",
+	fr: "une douleur" },
+	
+	{ en: "the flu",
+	fr: "la grippe" },
+
+	{ en: "a symptom",
+	fr: "un symptôme" },
+	
+	{ en: "an aspirin",
+	fr: "une aspirine" },
+	
+	{ en: "(a) medication",
+	fr: "un médicament" },
+	
+	{ en: "a prescription",
+	fr: "un ordonnance" },
+	
+	{ en: "the emergency room",
+	fr: "les urgences" },
+	
+	{ en: "depressed",
+	fr: "déprimé" },
+	
+	{ en: "serious",
+	fr: "grave" },
+	
+	{ en: "healthy",
+	fr: "sain" },
+	
+	{ en: "the fever",
+	fr: "la fièvre" },
+	
+	{ en: "a shot",
+	fr: "une piqûre" },
+	
+	{ en: "a nurse",
+	fr: "un infirmier" },
+	
+	{ en: "to exercise",
+	fr: "faire de l'exercise" },
+	
+	{ en: "a pill",
+	fr: "une pilule" },
+	
+	{ en: "a patient",
+	fr: "un patient" },
+	
+	{ en: "to cough",
+	fr: "tousser" },
+	
+	{ en: "a cold",
+	fr: "un rhume" },
+	
+	{ en: "an injury",
+	fr: "une blessure" },
+	
+	{ en: "to sneeze",
+	fr: "éternuer" },
+	
+	{ en: "achoo",
+	fr: "atchoum" },
+	
+	{ en: "backache",
+	fr: "mal au dos" },
+	
+	{ en: "headache",
+	fr: "mal à la tête" },
+	
+	{ en: "stomachache",
+	fr: "mal au ventre" }
+	
+];
---
+++ french_lists_11.js
@@ -0,0 +1,402 @@
+var list_errands = [
+
+	{ en: "to accompany",
+	fr: "accompagner" },
+
+	{ en: "to have a bank account",
+	fr: "avoir un compte bancaire" },
+
+	{ en: "to deposit money",
+	fr: "déposer de l'argent" },
+
+	{ en: "to  borrow",
+	fr: "emprunter" },
+
+	{ en: "to pay by credit card",
+	fr: "payer par carte" },
+
+	{ en: "to pay in cash",
+	fr: "payer en liquide" },
+
+	{ en: "to pay by cheque",
+	fr: "payer par chèque "},
+
+	{ en: "to fill out a form",
+	fr: "remplir un formulaire" },
+
+	{ en: "to withdraw money",
+	fr: "retirer de l'argent" },
+
+	{ en: "to sign",
+	fr: "signer" },
+
+	{ en: "an address",
+	fr: "une adresse" },
+
+	{ en: "a postcard",
+	fr: "une carte postale" },
+
+	{ en: "an envelope",
+	fr: "une enveloppe" },
+
+	{ en: "a stamp",
+	fr: "un timbre" },
+
+	{ en: "a boutique or store",
+	fr: "une boutique" },
+
+	{ en: "a cafe or restaurant",
+	fr: "une brasserie" },
+
+	{ en: "a laundromat",
+	fr: "une laverie" },
+
+	{ en: "a town hall or mayor's office",
+	fr: "une mairie" },
+
+	{ en: "a checking account",
+	fr: "un compte-chèques" },
+
+	{ en: "a savings account",
+	fr: "un compte d'épargne" },
+
+	{ en: "an expenditure",
+	fr: "une dépense" },
+
+	{ en: "some coins",
+	fr: "des pieces de monnaie" },
+
+	{ en: "some change",
+	fr: "de la monnaie" },
+
+	{ en: "closed (masculine)",
+	fr: "férme" },
+
+	{ en: "open (masculine)",
+	fr: "ouverte" },
+
+	{ en: "a jewellery store",
+	fr: "une bijouterie" },
+
+	{ en: "a post office",
+	fr: "un bureau de poste" },
+
+	{ en: "a package",
+	fr: "un colis" },
+
+	{ en: "a mailbox",
+	fr: "une boîte aux letters" },
+
+	{ en: "to send (a letter)",
+	fr: "poster" },
+
+	{ en: "a cybercafe",
+	fr: "un cybercafé" },
+
+	{ en: "a paper store",
+	fr: "une papeterie" },
+
+	{ en: "a mailman",
+	fr: "un facteur" },
+
+	{ en: "the mail",
+	fr: "le courrier" },
+
+	{ en: "a beauty salon",
+	fr: "un salon de beauté" },
+
+	{ en: "a bank",
+	fr: "une banque" },
+
+	{ en: "the till",
+	fr: "le guichet" },
+
+	{ en: "a bill (money)",
+	fr: "un billet" },
+
+	{ en: "an ATM",
+	fr: "un distributeur automatique" },
+
+	{ en: "to line up",
+	fr: "faire la queue" },
+
+	{ en: "a newspaper stand",
+	fr: "un marchand de journaux" }
+	
+];
+
+var list_travel = [
+	
+	{ en: "to continue",
+	fr: "continuer" },
+
+	{ en: "to move (change location)",
+	fr: "se déplacer" },
+
+	{ en: "to follow",
+	fr: "suivre" },
+
+	{ en: "to turn",
+	fr: "tourner" },
+
+	{ en: "to cross",
+	fr: "traverser" },
+
+	{ en: "a corner",
+	fr: "un angle" },
+
+	{ en: "an avenue",
+	fr: "une avenue" },
+
+	{ en: "a building",
+	fr: "un bâtiment" },
+
+	{ en: "a boulevard",
+	fr: "un boulevard" },
+
+	{ en: "a way or path",
+	fr: "un chemin" },
+
+	{ en: "a corner",
+	fr: "un coin" },
+
+	{ en: "a direction (instruction)",
+	fr: "une indication" },
+
+	{ en: "a tourist office",
+	fr: "un office du tourisme" },
+
+	{ en: "at the end of",
+	fr: "au bout de" },
+
+	{ en: "at th corner of",
+	fr: "au coin de" },
+
+	{ en: "around",
+	fr: "autour" },
+
+	{ en: "until",
+	fr: "jusqu'à" },
+
+	{ en: "close to",
+	fr: "près de" },
+
+	{ en: "straight ahead",
+	fr: "tout droit" },
+
+	{ en: "a bridge",
+	fr: "un pont" },
+
+	{ en: "to go up",
+	fr: "monter" },
+
+	{ en: "to descend",
+	fr: "descendre" },
+
+	{ en: "lost (masculine)",
+	fr: "perdu" },
+
+	{ en: "a statue",
+	fr: "une statue" },
+
+	{ en: "a fountain",
+	fr: "une fontaine" },
+
+	{ en: "to orient oneself",
+	fr: "s'orienter" },
+
+	{ en: "a phone booth",
+	fr: "une cabine téléphonique" },
+
+	{ en: "a bench",
+	fr: "un banc" },
+
+	{ en: "a traffic light",
+	fr: "un feu de signalisation" },
+
+	{ en: "an intersection",
+	fr: "un carrefour" },
+
+	{ en: "a road",
+	fr: "une rue" }
+
+];
+
+var list_stems = [
+
+	{ en: "acheter",
+	fr: "achèter" },
+
+	{ en: "nettoyer",
+	fr: "nettoier" },
+
+	{ en: "appeler",
+	fr: "appeller" },
+
+	{ en: "aller",
+	fr: "ir" },
+
+	{ en: "apercevoir",
+	fr: "apercevr" },
+
+	{ en: "avoir",
+	fr: "aur" },
+
+	{ en: "devoir",
+	fr: "devr" },
+
+	{ en: "envoyer",
+	fr: "enverr" },
+
+	{ en: "être",
+	fr: "ser" },
+
+	{ en: "faire",
+	fr: "fer" },
+
+	{ en: "pouvoir",
+	fr: "pourr" },
+
+	{ en: "recevoir",
+	fr: "recevr" },
+
+	{ en: "savoir",
+	fr: "saur" },
+
+	{ en: "venir",
+	fr: "viendr" },
+
+	{ en: "vouloir",
+	fr: "voudr" },
+
+	{ en: "il faut",
+	fr: "il faudra" },
+
+	{ en: "il pleut",
+	fr: "il pleuvra" },
+
+	{ en: "voir",
+	fr: "verr" },
+
+	{ en: "tenir",
+	fr: "tiendr" },
+
+	{ en: "maintenir",
+	fr: "maintiendr" }
+
+];
+
+var list_art = [
+	
+	{ en: "to play a role",
+	fr: "jouer un role" },
+
+	{ en: "to present",
+	fr: "presenter" },
+
+	{ en: "to take advantage of",
+	fr: "profiter de" },
+
+	{ en: "(an) applause",
+	fr: "un applaudissement" },
+
+	{ en: "a song",
+	fr: "une chanson" },
+
+	{ en: "a choir or chorus",
+	fr: "un cheour" },
+
+	{ en: "a comedy",
+	fr: "une comedie" },
+
+	{ en: "a composer",
+	fr: "un compositeur" },
+
+	{ en: "a concert",
+	fr: "un concert" },
+
+	{ en: "a dance",
+	fr: "une danse" },
+
+	{ en: "a playwright",
+	fr: "un dramaturge" },
+
+	{ en: "an intermission",
+	fr: "un entracte" },
+
+	{ en: "a member",
+	fr: "un membre" },
+
+	{ en: "a director (of a play or show)",
+	fr: "un metteur en scene" },
+
+	{ en: "a character",
+	fr: "un personnage" },
+
+	{ en: "a play",
+	fr: "un piece de theatre" },
+
+	{ en: "a director (of a movie)",
+	fr: "un realisateur" },
+
+	{ en: "a show or screening",
+	fr: "une seance" },
+
+	{ en: "a company or troop",
+	fr: "une troupe" },
+
+	{ en: "the beginning",
+	fr: "le debut" },
+
+	{ en: "the end",
+	fr: "la fin" },
+
+	{ en: "a genre",
+	fr: "un genre" },
+
+	{ en: "a sort or type",
+	fr: "une sorte" },
+
+	{ en: "famous",
+	fr: "celebre" },
+
+	{ en: "a tragedie",
+	fr: "une tragedie" },
+
+	{ en: "a dancer",
+	fr: "un danseur" },
+
+	{ en: "a spectator (male)",
+	fr: "un spectateur" },
+
+	{ en: "a spectator (female)",
+	fr: "une specatrice" },
+
+	{ en: "to applaud",
+	fr: "applaudir" },
+
+	{ en: "a piano",
+	fr: "un piano" },
+
+	{ en: "a guitar",
+	fr: "une guitare" },
+
+	{ en: "an orchestra",
+	fr: "un orchestre" },
+
+	{ en: "the drums",
+	fr: "la batterie" },
+
+	{ en: "to play music",
+	fr: "faire de la musique" },
+
+	{ en: "to play (music)",
+	fr: "jouer" },
+
+	{ en: "a violin",
+	fr: "le violon" },
+
+	{ en: "an opera",
+	fr: "un opera" }
+
+];
